6ecccd8804dbbe6290b6775caddf06204f6d5d5b
Ben Harloe <ben.harloe@gmail.com>
2018-03-27 11:44:03 -0700

    provisioning dynamodb table in yaml

--- create/index.js
+++ create/index.js
@@ -1,10 +1,11 @@
 'use strict';
 const querystring = require('querystring');
 
+const prefix = "mysite.com";
+
 module.exports.handler = (event, context, callback) => {
     console.log(JSON.stringify(event));
     const submitted = querystring.parse(event.body).link;
-    const prefix = event.headers.Referer || 'http://mysite.com';
     console.log('URL submitted: ' + submitted);
     callback(
         null,
